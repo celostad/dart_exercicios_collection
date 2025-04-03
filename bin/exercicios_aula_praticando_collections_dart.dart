e36bea044b3999b48a7fa60553479779768d189b
Marcelo Tadim <marcelo.tadim@lowcost.com.br>
2025-04-03 19:56:08 -0300

    Exercicios Collection - 05.Listando funcionários

--- bin/exercicios_aula_praticando_collections_dart.dart
+++ bin/exercicios_aula_praticando_collections_dart.dart
@@ -1,26 +1,37 @@
 
 /*
-A Dev Kanban está desenvolvendo um aplicativo de produtividade
- onde as pessoas usuárias podem verificar suas tarefas.
+Em um sistema administrativo, cada funcionário é representado por um ID
+ (identificador único) em forma de inteiro. 
+
+ Crie uma função que receba a lista de IDs dos funcionários e exiba apenas os IDs que são pares.
 */
 
+
+import 'dart:ffi';
 
 void main() { 
 
 // Este é apenas um exemplo, os valores que a lista possui podem variar. 
 
-List<String> tarefas = ["Estudar", "Comprar mantimentos"]; 
+List<int> idsFuncionarios = [1, 2, 3, 4, 5, 6, 7, 8, 9]; 
 
 
-exibirListaTarefa(tarefas);
+
+exibirIdsPares(idsFuncionarios);
 }
 
-void exibirListaTarefa(List<String> tarefas) { 
+// A função verifica se o ID é par usando id % 2 == 0 
+// e exibe apenas os valores que atendem a essa condição.
+void exibirIdsPares(List<int> idsFuncionarios) { 
 
-  for (String tarefa in tarefas) { 
+  for (int id in idsFuncionarios) { 
 
-      print(tarefa); 
+    if(id  % 2 == 0){//pares
+      print(id); 
+    }
 
   } 
 
 }
+// Outro exemplo:
+// var pares = [1, 2, 3, 4, 5].where((n) => n % 2 == 0).toList();
