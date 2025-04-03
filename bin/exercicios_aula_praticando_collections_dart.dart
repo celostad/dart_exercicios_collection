bf7bd6461963c86598c3bebcbc179c3ccf62b96e
Marcelo Tadim <marcelo.tadim@lowcost.com.br>
2025-04-03 19:40:18 -0300

    Exercicios Collection - first commit

---
+++ bin/exercicios_aula_praticando_collections_dart.dart
@@ -0,0 +1,5 @@
+import 'package:exercicios_aula_praticando_collections_dart/exercicios_aula_praticando_collections_dart.dart' as exercicios_aula_praticando_collections_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${exercicios_aula_praticando_collections_dart.calculate()}!');
+}
---
+++ test/exercicios_aula_praticando_collections_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:exercicios_aula_praticando_collections_dart/exercicios_aula_praticando_collections_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
