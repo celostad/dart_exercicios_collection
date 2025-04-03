883cd6c34baf6f8be5bd8eba5676b9e9c4a2b1f6
Marcelo Tadim <marcelo.tadim@lowcost.com.br>
2025-04-03 20:14:35 -0300

    Exercicios Collection - 06.Contando produtos em um armazém

--- bin/exercicios_aula_praticando_collections_dart.dart
+++ bin/exercicios_aula_praticando_collections_dart.dart
@@ -1,37 +1,30 @@
 
 /*
-Em um sistema administrativo, cada funcionário é representado por um ID
- (identificador único) em forma de inteiro. 
-
- Crie uma função que receba a lista de IDs dos funcionários e exiba apenas os IDs que são pares.
+  Uma empresa está gerenciando o inventário de seus produtos e precisa de uma solução
+  para monitorar a quantidade de unidades de certos itens em seu estoque. 
+  Para isso, você deve criar uma função que, dado uma lista de produtos, 
+  conte quantas vezes o item "maçã" aparece na lista de produtos. 
 */
-
-
-import 'dart:ffi';
 
 void main() { 
 
 // Este é apenas um exemplo, os valores que a lista possui podem variar. 
 
-List<int> idsFuncionarios = [1, 2, 3, 4, 5, 6, 7, 8, 9]; 
+List<String> produtos = ["maçã", "banana", "maçã", "laranja", "maçã"]; 
 
+print(contarMacas(produtos));
 
-
-exibirIdsPares(idsFuncionarios);
 }
 
-// A função verifica se o ID é par usando id % 2 == 0 
-// e exibe apenas os valores que atendem a essa condição.
-void exibirIdsPares(List<int> idsFuncionarios) { 
 
-  for (int id in idsFuncionarios) { 
-
-    if(id  % 2 == 0){//pares
-      print(id); 
+int contarMacas(List<String> produtos) { 
+  int totalMaca = 0;
+  for (String produto in produtos) {
+    if(produto == "maçã"){
+      totalMaca++;
     }
+  }
 
-  } 
+  return totalMaca;
 
 }
-// Outro exemplo:
-// var pares = [1, 2, 3, 4, 5].where((n) => n % 2 == 0).toList();
